0e02e3bdd29c543c7ed54a545c5df8f900824841
Eric Cheng <cheng.eri@northeastern.edu>
2020-07-29 18:29:29 -0400

    undo changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+//import '../styles/buttons.css'
+
 
 class Home extends React.Component {
 
   render() {
     return (
-      <div><h1>Welcome to Silver ScrumBoard</h1>
+      <div><h1>Welcome to Silver ScrumBoadrd</h1>
+      <img src={table} alt="Logo" />;
         <span>
           <button type="button" class="btn btn-primary">My Dashboard</button>
           <button type="button" class="btn btn-primary">BackLog</button>
