ae00b8c8a39441c75fa6d1faa17749fdcf714570
Eric Cheng <cheng.eri@northeastern.edu>
2020-07-29 20:11:08 -0400

    route to dashboard and backlog breakpoint - ask ta for second look

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 // import '../styles/buttons.css'
 // import Link from 'react-router-dom'
-import dashboard from './Dashboard'
+import Dashboard from './Dashboard.jsx'
+import Issues from './IssueList.jsx'
 
 class Home extends React.Component {
 
   render() {
     return (
       <div><h1>Welcome to Silver ScrumBoadrd</h1>
-      <img src={table} alt="Logo" />;
         <span>
           {/*<Link to={`/dashboard`}>*/}
           <button 
             type="button" 
             class="btn btn-primary" 
-            onClick={dashboard}
+            onClick={Dashboard}
             >
             My Dashboard
           </button>
           {/*</Link>*/}
-          <button type="button" class="btn btn-primary">BackLog</button>
+          <button type="button" class="btn btn-primary" onClick={Issues}>BackLog</button>
         </span>
       </div>
     );
