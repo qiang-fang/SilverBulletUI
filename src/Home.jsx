bcc75c3556897223abf47806a22a5236f67c3372
qiangfang <fang.q@husky.neu.edu>
2020-07-31 20:09:47 -0400

    update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
+
 //import './buttons.css'
 //import image from '../images/Acadia.jpg'
+
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import Dashboard from './Dashboard.jsx'
+import Dashboard from './DashboardTicket.jsx'
 import Issues from './IssueList.jsx'
 
 class Home extends React.Component {
-
   render() {
     return (
       <div><h1>Welcome to Silver ScrumBoard</h1>
         <span>
           <Link to={`/dashboard`}>
-          <button 
-            type="button" 
-            class="btn btn-primary" 
-            onClick={Dashboard}
+            <button
+              type="button"
+              class="btn btn-primary"
+              onClick={Dashboard}
             >
-            My Dashboard
+              My Dashboard
           </button>
           </Link>
           <Link to={`/issues`}>
-            <button 
-            type="button" 
-            class="btn btn-primary" 
-            onClick={Issues}
+            <button
+              type="button"
+              class="btn btn-primary"
+              onClick={Issues}
             >
-            BackLog
+              BackLog
           </button>
           </Link>
         </span>
