aa6961e247f676b9e0367c040c4883cea8b5cfb8
Eric Cheng <cheng.eric11@gmail.com>
2020-08-03 20:12:03 -0400

    embedded css in render

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,34 +1,45 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-//import './buttons.css'
-//import image from '../images/Acadia.jpg'
+// import './buttons.css';
+// import image from '../images/Acadia.jpg'
 
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import Dashboard from './DashboardTicket.jsx'
-import Issues from './IssueList.jsx'
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Dashboard from './DashboardTicket.jsx';
+import Issues from './IssueList.jsx';
 
 class Home extends React.Component {
   render() {
+    // const image = require('../images/BackLog.png');
+    const mystyle = {
+      width: '100%',
+      height: '100%',
+      'text-align': 'center',
+    };
     return (
-      <div><h1>Welcome to Silver ScrumBoard</h1>
+      <div style={mystyle}>
+        <h1>Welcome to Silver ScrumBoard</h1>
         <span>
-          <Link to={`/dashboard`}>
+          <Link to="/dashboard">
             <button
               type="button"
-              class="btn btn-primary"
+              className="btn btn-primary"
               onClick={Dashboard}
             >
               My Dashboard
-          </button>
+            </button>
           </Link>
-          <Link to={`/issues`}>
+        </span>
+          &nbsp;
+        <span>
+          <Link to="/issues">
             <button
               type="button"
-              class="btn btn-primary"
+              className="btn btn-primary"
               onClick={Issues}
             >
               BackLog
-          </button>
+            </button>
           </Link>
         </span>
       </div>
